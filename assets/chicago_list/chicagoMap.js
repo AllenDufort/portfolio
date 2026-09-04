94088d9bc671a503970bad6158015495f406d6e4
Allen Dufort <Allen.Dufort@ibm.com>
2026-09-04 10:15:03 -0500

    Update chicagoMap.js

--- assets/chicago_list/chicagoMap.js
+++ assets/chicago_list/chicagoMap.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const map = L.map('map', { zoomControl: false }).setView(CHICAGO_CENTER, 11);
     let userLocationMarker = null;
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     // Custom zoom control
